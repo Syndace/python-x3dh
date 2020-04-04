c125aba05688e867e69e205ab08bb72ac4740ebb
Tim Henkes <me@syndace.dev>
2020-04-04 22:42:10 +0200

    type checking and linting setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup, find_packages
-
+# pylint: disable=exec-used
 import os
-import sys
+from typing import Dict, Union, List
+
+from setuptools import setup, find_packages # type: ignore[import]
 
 source_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "x3dh")
 
-version = {}
+version_scope: Dict[str, Dict[str, str]] = {}
 with open(os.path.join(source_root, "version.py")) as f:
-	exec(f.read(), version)
-version = version["__version__"]
+    exec(f.read(), version_scope)
+version = version_scope["__version__"]
 
-project = {}
+project_scope: Dict[str, Dict[str, Union[str, List[str]]]] = {}
 with open(os.path.join(source_root, "project.py")) as f:
-	exec(f.read(), project)
-project = project["project"]
+    exec(f.read(), project_scope)
+project = project_scope["project"]
 
 with open("README.md") as f:
     long_description = f.read()
@@ -28,7 +29,10 @@ classifiers = [
 
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8"
+    "Programming Language :: Python :: 3.8",
+
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy"
 ]
 
 classifiers.extend(project["categories"])
