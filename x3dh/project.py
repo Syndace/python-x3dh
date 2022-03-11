9259e7fb18549183a8b9fca9cc04d764de514100
Tim Henkes <tim.henkes@hs-rm.de>
2022-03-11 11:49:40 +0100

    Copyright year bump

--- x3dh/project.py
+++ x3dh/project.py
@@ -2,7 +2,7 @@ project = {
     "name"         : "X3DH",
     "description"  : "A Python implementation of the Extended Triple Diffie-Hellman key agreement protocol.",
     "url"          : "https://github.com/Syndace/python-x3dh",
-    "year"         : "2020",
+    "year"         : "2022",
     "author"       : "Tim Henkes (Syndace)",
     "author_email" : "me@syndace.dev",
     "categories"   : [
