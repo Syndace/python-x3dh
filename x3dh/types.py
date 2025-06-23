42e0265fc2452174ec8f6a5ac97cfd8be8672132
Tim Henkes <me@syndace.dev>
2025-06-23 09:09:55 +0200

    Remove unused import

--- x3dh/types.py
+++ x3dh/types.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import enum
-from typing import Dict, FrozenSet, List, Mapping, NamedTuple, Optional, Union
+from typing import Dict, FrozenSet, List, NamedTuple, Optional, Union
 from typing_extensions import TypeAlias
 
 
